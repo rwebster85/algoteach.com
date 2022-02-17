608c09c920f3fe6eae26dd78ba2d9056edd56f22
Richard Webster <2019956@chester.ac.uk>
2022-02-17 11:41:52 +0000

    Comments added

--- tools/RichPHPTests/Includes/FileLoader.php
+++ tools/RichPHPTests/Includes/FileLoader.php
@@ -4,17 +4,35 @@ declare(strict_types=1);
 
 namespace RichPHPTests;
 
+use function file_exists;
+
 final class FileLoader
 {
+    /**
+     * Array of file path strings for loading.
+     * 
+     * @var string[]
+     */
     private array $files = [];
 
-    public function __construct(array $files)
+    /**
+     * Accepts an array of strings as file paths to load.
+     * 
+     * @param string[] $files
+     */
+    public function __construct(string ...$files)
     {
-        foreach ($files as $file) {
-            $this->files[] = $file;
-        }
+        $this->files = $files;
     }
 
+    /**
+     * Iterates through $files and passes each to requireFile().
+     * 
+     * @uses FileLoader::$files
+     * @uses FileLoader::requireFile()
+     * 
+     * @return void
+     */
     public function loadFiles(): void
     {
         foreach ($this->files as $file) {
@@ -22,7 +40,16 @@ final class FileLoader
         }
     }
 
-    private function requireFile($file): void
+    /**
+     * Checks if a file exists then requires it.
+     * 
+     * @param string $file
+     * 
+     * @uses \file_exists
+     * 
+     * @return void
+     */
+    private function requireFile(string $file): void
     {
         if (file_exists($file)) {
             require_once $file;
