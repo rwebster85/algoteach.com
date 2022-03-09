840f15ff2553c7b9fbca76bedbc82f3467b5ab42
Richard Webster <2019956@chester.ac.uk>
2022-03-09 14:20:03 +0000

    Fixed js file

--- plugins/rich-algorithms/Assets/JS/rich-algo.js
+++ plugins/rich-algorithms/Assets/JS/rich-algo.js
@@ -1,25 +1,5 @@
 (function($) {
     $(document).ready(function() {
-        /**
-         * Ensure the code examples wrapper height remains stable when sorting them.
-         */
-        $(document).on('mousedown', '.rich-algo-sort-button', function() {
-            $('#rich-algo-example-sortable').height(sort_height);
-        }).on('mouseup', '.rich-algo-sort-button', function() {
-            $('#rich-algo-example-sortable').height('');
-        });
-        $(document).on('mouseup', '#rich-algo-example-sortable textarea', function() {
-            sort_height = $('#rich-algo-example-sortable').height();
-        });
-
-        $(document).on('click', '.rich-algo-example-remove', function(e) {
-            e.preventDefault();
-            if (window.confirm('Remove this code example?')) {
-                $(this).parent().parent().remove();
-                resetWrapper();
-            }
-        });
-
         $(document).on('click', '.rich-algo-copy', function(e) {
             e.preventDefault();
             var element = $(this);
