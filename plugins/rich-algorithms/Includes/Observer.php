98309787a934798ff34d704c18a8adaa63d27663
Richard Webster <2019956@chester.ac.uk>
2022-03-12 14:36:32 +0000

    Added observer object and interface.

---
+++ plugins/rich-algorithms/Includes/Observer.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Rich Algorithms.
+ *
+ * Copyright (c) Richard Webster
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace RichWeb\Algorithms;
+
+use RichWeb\Algorithms\Interfaces\ObserverInterface;
+
+class Observer implements ObserverInterface
+{
+    final function register(string $event, object $caller, string $callback, int $priority = 10, int $args = 1): void
+    {
+        add_action($event, [$caller, $callback], $priority, $args);
+    }
+}
