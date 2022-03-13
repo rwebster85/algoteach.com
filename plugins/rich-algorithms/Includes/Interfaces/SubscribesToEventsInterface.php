da97603e8179072d4404a0a853ca65d87ae50b59
Richard Webster <2019956@chester.ac.uk>
2022-03-13 14:38:15 +0000

    Added comment

--- plugins/rich-algorithms/Includes/Interfaces/SubscribesToEventsInterface.php
+++ plugins/rich-algorithms/Includes/Interfaces/SubscribesToEventsInterface.php
@@ -20,6 +20,10 @@ interface SubscribesToEventsInterface
     /**
      * Method containing the events this object will subscribe to.
      * 
+     * @param EventSubscriberInterface $subscriber The subscriber object that carries out the subscription logic.
+     * 
+     * @see EventSubscriberInterface Event subscriber class.
+     * 
      * @return void
      */
     public function subscribeToEvents(EventSubscriberInterface $subscriber): void;
