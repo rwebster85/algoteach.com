0e3efad59720ff49f8c3a0c54a5de269b229b45b
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-03-16 10:55:14 +0000

    Added point in polygon base

---
+++ plugins/rich-algorithms/Algorithms/PointInPolygon/PointInPolygon.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of Rich Algorithms.
+ *
+ * Copyright (c) Richard Webster
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace RichWeb\Algorithms\Algorithm;
+
+use RichWeb\Algorithms\Abstracts\AbstractAlgorithm;
+
+use const RichWeb\Algorithms\PLUGIN_FILE;
+
+final class PointInPolygon extends AbstractAlgorithm
+{
+    protected function load(): void
+    {
+        //
+    }
+}
