0954f0e93c7470ab11c362255d25a4554fc1b7d9
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-05 14:50:21 +0100

    Added unit tests for CodeExamplesLoader

---
+++ plugins/rich-algorithms/Tests/UnitTests/Models/TestCodeExamplesLoader.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace RichAlgoUnitTests;
+
+use RichPHPTests\Attributes;
+use RichPHPTests\TestCase;
+use RichWeb\Algorithms\CodeExamples\CodeExampleModel;
+use RichWeb\Algorithms\CodeExamples\CodeExamplesLoader;
+use RichWeb\Algorithms\Interfaces\CodeExamplesLoaderInterface;
+
+class TestCodeExamplesLoader extends TestCase
+{
+    private array $languages;
+
+    public function setUpClass(): void
+    {
+        $this->languages = [
+            'php' => 'PHP',
+            'js'  => 'JavaScript'
+        ];
+    }
+
+    public function testBuildCodeExamplesOnce(): void
+    {
+        $examples = [
+            [
+                'code' => '<?php echo hello; ?>',
+                'lang' => 'php',
+                'vers' => '1.5',
+                'info' => 'This is a PHP example.'
+            ]
+        ];
+
+        $loader = new CodeExamplesLoader($this->languages);
+        $loader->buildCodeExamples($examples);
+        test($loader->getCodeExamples())->arrayIsSize(1);
+    }
+
+    public function testBuildCodeExamplesTwo(): void
+    {
+        $examples = [
+            [
+                'code' => '<?php echo hello; ?>',
+                'lang' => 'php',
+                'vers' => '1.5',
+                'info' => 'This is a PHP example.'
+            ],
+            [
+                'code' => 'console.log("hello")',
+                'lang' => 'js',
+                'vers' => '1.0',
+                'info' => 'This is a JavaScript example.'
+            ],
+        ];
+
+        $loader = new CodeExamplesLoader($this->languages);
+        $loader->buildCodeExamples($examples);
+        test($loader->getCodeExamples())->arrayIsSize(2);
+    }
+
+    public function testGetCodeExamplesContainsModel(): void
+    {
+        $examples = [
+            [
+                'code' => '<?php echo hello; ?>',
+                'lang' => 'php',
+                'vers' => '1.5',
+                'info' => 'This is a PHP example.'
+            ],
+            [
+                'code' => 'console.log("hello")',
+                'lang' => 'js',
+                'vers' => '1.0',
+                'info' => 'This is a JavaScript example.'
+            ],
+        ];
+
+        $loader = new CodeExamplesLoader($this->languages);
+        $loader->buildCodeExamples($examples);
+        $example_one = $loader->getCodeExamples()[0];
+        test($example_one)->isObjectOfType('RichWeb\Algorithms\CodeExamples\CodeExampleModel');
+    }
+}
