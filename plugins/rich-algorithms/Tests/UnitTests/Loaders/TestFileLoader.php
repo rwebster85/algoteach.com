c6072a0986ff16607779a08ef77de9e1613792b2
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-07 11:56:09 +0100

    Alignment + additional tests

--- plugins/rich-algorithms/Tests/UnitTests/Loaders/TestFileLoader.php
+++ plugins/rich-algorithms/Tests/UnitTests/Loaders/TestFileLoader.php
@@ -4,16 +4,20 @@ namespace RichAlgoUnitTests\Loaders;
 
 use RichPHPTests\TestCase;
 use RichWeb\Algorithms\Loaders\FileLoader;
+use stdClass;
 
 use const DIRECTORY_SEPARATOR as SEP;
 
 class TestFileLoader extends TestCase
 {
-    public function testFileLoaderLoadsFile(): void
+    public function testLoadsFile(): void
     {
         $files = [__DIR__ . SEP . 'MockFileLoaderClassFile.php'];
         (new FileLoader(...$files))->loadFiles();
-        $exists = class_exists(__NAMESPACE__ . '\MockFileLoaderClassFile', false);
+        $exists = class_exists(
+            __NAMESPACE__ . '\MockFileLoaderClassFile',
+            false
+        );
         test($exists)->isTrue();
     }
 
@@ -22,11 +26,41 @@ class TestFileLoader extends TestCase
      * 
      * @return void
      */
-    public function testFileLoaderFileNotExists(): void
+    public function testFileNotExists(): void
     {
         $files = [__DIR__ . SEP . 'DoesNotExist.php'];
         (new FileLoader(...$files))->loadFiles();
-        $exists = class_exists(__NAMESPACE__ . '\DoesNotExist', false);
+        $exists = class_exists(
+            __NAMESPACE__ . '\DoesNotExist',
+            false
+        );
         test($exists)->isFalse();
+    }
+
+    public function testRaisesErrorIncorrectTypeSingle(): void
+    {
+        $error = false;
+
+        try {
+            $loader = new FileLoader(new stdClass());
+        } catch (\Throwable $th) {
+            $error = true;
+        }
+        
+        test($error)->isTrue();
+    }
+
+    public function testRaisesErrorIncorrectTypeMultiple(): void
+    {
+        $error = false;
+
+        try {
+            $files = [new stdClass(), new stdClass()];
+            $loader = new FileLoader(...$files);
+        } catch (\Throwable $th) {
+            $error = true;
+        }
+        
+        test($error)->isTrue();
     }
 }
