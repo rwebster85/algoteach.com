175a5c973c652aa2f3b8f17cbe97b1ca77b57646
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-11 14:22:25 +0100

    Theme customisations

---
+++ themes/algo-teach/functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+add_action('algo_homepage', 'storefront_homepage_content', 10);
+add_action('algo_homepage', 'storefront_product_categories', 20);
+add_action('algo_homepage', 'storefront_recent_products', 30);
+add_action('algo_homepage', 'storefront_featured_products', 40);
+add_action('algo_homepage', 'storefront_popular_products', 50);
+add_action('algo_homepage', 'storefront_on_sale_products', 60);
+add_action('algo_homepage', 'storefront_best_selling_products', 70);
+
+add_action('init', function() {
+    remove_action('storefront_header', 'storefront_product_search', 40);
+});
