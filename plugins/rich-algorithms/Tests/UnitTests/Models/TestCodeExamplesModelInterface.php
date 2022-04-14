7299688d97f9c5940181c438e73b8cab0f5eba4c
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-14 11:52:11 +0100

    Added missing declare strict and copyright

--- plugins/rich-algorithms/Tests/UnitTests/Models/TestCodeExamplesModelInterface.php
+++ plugins/rich-algorithms/Tests/UnitTests/Models/TestCodeExamplesModelInterface.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of Rich Algorithms.
+ *
+ * Copyright (c) Richard Webster
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace RichAlgoUnitTests\Models;
 
 use RichPHPTests\Attributes;
