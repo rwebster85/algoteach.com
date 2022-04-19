8db407db56a771329244bbf534a3a4dbb2f0a14c
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-19 10:14:38 +0100

    New homepage

---
+++ themes/algo-teach/homepage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the homepage.
+ *
+ * This page template will display any functions hooked into the `homepage` action.
+ * By default this includes a variety of product displays and the page content itself. To change the order or toggle these components
+ * use the Homepage Control plugin.
+ * https://wordpress.org/plugins/homepage-control/
+ *
+ * Template name: Algo Homepage
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php do_action('algo_homepage'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+do_action( 'storefront_sidebar' );
+get_footer();
