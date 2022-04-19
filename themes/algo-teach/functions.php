85dfa05ac3d7292497b58662cc70fb7579d3caa1
Richard Webster <16655270+rwebster85@users.noreply.github.com>
2022-04-19 12:38:15 +0100

    Rearranged actions.

--- themes/algo-teach/functions.php
+++ themes/algo-teach/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+add_action('init', function() {
+    remove_action('storefront_header', 'storefront_product_search', 40);
+    remove_action('storefront_footer', 'storefront_handheld_footer_bar', 999);
+    add_action('wp_enqueue_scripts', function() {
+        $timestamp = time();
+        $style_url = get_stylesheet_directory_uri() . '/Assets/CSS/style.css';
+        wp_enqueue_style('rich-algo-theme-style', $style_url, [], $timestamp);
+    });
+});
+
 add_action('algo_homepage', function() {
     while ( have_posts() ) {
         the_post();
@@ -7,18 +17,4 @@ add_action('algo_homepage', function() {
     }
 }, 10);
 
-add_action('init', function() {
-    remove_action('storefront_header', 'storefront_product_search', 40);
-    remove_action('storefront_footer', 'storefront_handheld_footer_bar', 999);
-});
-
 add_filter('wp_lazy_loading_enabled', '__return_false');
-
-
-add_action('init', function() {
-    add_action('wp_enqueue_scripts', function() {
-        $timestamp = time();
-        $style_url = get_stylesheet_directory_uri() . '/Assets/CSS/style.css';
-        wp_enqueue_style('rich-algo-theme-style', $style_url, [], $timestamp);
-    });
-});
